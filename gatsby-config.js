4f55cf5c3cdf3dbb681d26780560bae7e0619ea8
devolasvegas <devondaviau@yahoo.ca>
2019-07-03 16:49:35 -0400

    config client paths and add auth script

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,6 +32,12 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-plugin-create-client-paths`,
+      options: {
+        prefixes: [`/app/*`],
+      },
+    },
   ],
   developMiddleware: app => {
     app.use(
