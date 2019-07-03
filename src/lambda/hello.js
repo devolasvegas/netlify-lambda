c9a091f96f4d1308041e6741c326621de6b6efad
devolasvegas <devondaviau@yahoo.ca>
2019-07-03 14:26:16 -0400

    update the body msg

--- src/lambda/hello.js
+++ src/lambda/hello.js
@@ -1,3 +1,9 @@
 exports.handler = function(event, context, callback) {
-  callback(null, { statusCode: 200, body: JSON.stringify({ msg: "Hieee" }) })
+  callback(null, {
+    statusCode: 200,
+    body: JSON.stringify({
+      msg:
+        "Hieee. I am being pulled in using a lambda function and the fetch API.",
+    }),
+  })
 }
