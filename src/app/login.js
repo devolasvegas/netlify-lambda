9aad5155ffb04f90fbafdd9f590b0865bda15be7
devolasvegas <devondaviau@yahoo.ca>
2019-07-04 09:10:59 -0400

    add login component

---
+++ src/app/login.js
@@ -0,0 +1,16 @@
+import React from "react"
+import { navigate } from "gatsby"
+import { handleLogin, isLoggedIn } from "./services/auth"
+
+const Login = () => {
+  const handleSubmit = () => handleLogin(user => navigate("/app/profile"))
+
+  return (
+    <>
+      <h1>Log in</h1>
+      <button onClick={handleSubmit}>log in</button>
+    </>
+  )
+}
+
+export default Login
